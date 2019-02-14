2297f4eefd6f15b77057e95f46586e0f4bf8df7b
Billy Rieger <wrieger@protonmail.com>
2019-02-14 17:40:29 -0500

    Update .travis.yml

--- smeagol/src/node/impls/properties.rs
+++ smeagol/src/node/impls/properties.rs
@@ -1,14 +1,17 @@
 use crate::node::{NodeId, Store};
 
 impl NodeId {
+    /// Returns the level of the node.
     pub fn level(&self, store: &Store) -> u8 {
         store.get(*self).level
     }
 
+    /// Returns the population of the node.
     pub fn population(&self, store: &Store) -> u128 {
         store.get(*self).population
     }
 
+    /// Returns the minimum coordinate that can be used with the node.
     pub fn min_coord(&self, store: &Store) -> i64 {
         let level = self.level(store);
         if level == 64 {
@@ -18,6 +21,7 @@ impl NodeId {
         }
     }
 
+    /// Returns the maximum coordinate that can be used with the node.
     pub fn max_coord(&self, store: &Store) -> i64 {
         let level = self.level(store);
         if level == 64 {
